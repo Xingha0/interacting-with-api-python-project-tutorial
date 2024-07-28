2cd5660c4df77bba382764b03fa5d6a15c176abe
Jorge Alvarez Garcia <114688583+Xingha0@users.noreply.github.com>
2024-07-28 10:07:03 +0200

    craneo

--- src/app.py
+++ src/app.py
@@ -1,7 +1,36 @@
+
 import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy import SpotifyClientCredentials
 
-# load the .env file variables
 load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+sp = spotipy.Spotify(auth_manager= SpotifyClientCredentials(client_id, client_secret))
+
+
+id_artista = '05aVtfDzBvg9eVu9MAZPGD'
+respuesta = sp.artist_top_tracks(id_artista)
+tracks = respuesta['tracks']
+
+
+df_tracks = pd.DataFrame.from_records(tracks)
+df_tracks
+
+
+df_tracks.sort_values(['popularity'],inplace=True)
+df_tracks
+
+df_tracks.head(3)
+
+
+import matplotlib.pyplot as plt
+
+plt.scatter(x=df_tracks['popularity'],y=df_tracks['duration_ms'])
+plt.show
+
